68d7ad8b70bf99befc90754b5509126741d19ffe
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2025-08-29 09:40:23 +0530

    Simple css decoration in nav bar

--- frontend/src/Component/Navbar/Navbar.jsx
+++ frontend/src/Component/Navbar/Navbar.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 import logo from '../../assets/logo.png'
 import cart_icon from '../../assets/cart_icon.png'
 import './Navbar.css'
+
+
 function Navbar() {
   return (
+
     <div className="navbar">
+      
         <div className="nav-logo">
             <img src={logo} alt="" />
             <p>SHOPPER</p>
         </div>
+
         <ul className="nav-menu">
-            <li>shop</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li><a href="#Shop">Shop</a></li>
+            <li><a href="#Men">Men</a></li>
+            <li><a href="#Women">Women</a></li>
+            <li><a href="#Kids">Kids</a></li>
              </ul>
+
        <div className="nav-logo-cart">
         <button>LOGIN</button>
         <img src={cart_icon} alt="" />
         <div className="nav-cart-count">0</div>
        </div>
+
     </div>
     
   )
