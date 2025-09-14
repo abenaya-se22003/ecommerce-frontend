ed86e6deb703b4f5b329d310e2bed8e18aee7388
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2025-09-14 23:21:34 +0530

    Create Shop Ctegory pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,9 @@ import Product from './Component/Pages/Product';
 import Cart from './Component/Pages/Cart'
 import LoginSignup from './Component/Pages/LoginSignup';
 import Footer from './Component/Footer/Footer ';
+import men_banner from './assets/banner_mens.png'
+import women_banner from './assets/banner_women.png'
+import kid_banner from './assets/banner_kids.png'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,9 +24,9 @@ function App() {
     
       <Routes>
         <Route path='/' element={<Shop />} />
-        <Route path='/mens' element={<ShopCategory category="men" />} />
-        <Route path='/womens' element={<ShopCategory category="women" />} />
-        <Route path='/kids' element={<ShopCategory category="kid" />} />
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
         <Route path='/product' element={<Product />} />
         <Route path='/product/:productId' element={<Product />} />
         <Route path='/cart' element={<Cart />} />
--- frontend/src/Component/Pages/ShopCategory.jsx
+++ frontend/src/Component/Pages/ShopCategory.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./Css/shopCategory.css";
+import dropdown_icon from "../../assets/dropdown_icon.png";
+import Item from "../Item/item";
+import all_product from "../../assets/all_product";
 
-function ShopCategory() {
+function ShopCategory({ banner, category }) {
   return (
-    <div>ShopCategory</div>
-  )
+    <div className="shop-category">
+     
+      <img src={banner} alt="banner" />
+
+      <div className="shopcategory-indexSort">
+        <p>
+          Showing 1-12 <span> out of 36 products  </span>
+        </p>
+        <div className="shopcategory-sort">
+          Sort by<img src={dropdown_icon} alt="dropdown" />
+        </div>
+      </div>
+
+      {/* Products */}
+      <div className="shopcategory-products">
+        {all_product
+          .filter((item) => item.category === category)
+          .map((item, i) => (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
 
-export default ShopCategory
+export default ShopCategory;
