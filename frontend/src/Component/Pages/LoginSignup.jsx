585c53335ee023a498f2709956ebbaddf3c860f6
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2025-09-18 17:32:24 +0530

    Create login page

--- frontend/src/Component/Pages/LoginSignup.jsx
+++ frontend/src/Component/Pages/LoginSignup.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './Css/LoginSignup.css'
 
 function LoginSignup() {
   return (
-    <div><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat sint,<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum deserunt, aliquam fuga quaerat quo voluptas enim reprehenderit illum iste sint ea, aliquid dolore. Adipisci, praesentium esse sed rem hic facere.</p> similique doloribus tempora omnis officia corrupti sed aliquid minima quos ullam pariatur laudantium recusandae, quod quisquam repudiandae voluptate delectus repellat?</p></div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+            <h1>Sign Up</h1>
+            <input type="text" placeholder='Your Name' />
+            <input type="email" placeholder='Email Address' />
+            <input type="password" placeholder='Password' />
+     
+       <button>Continue</button> 
+       <div className="agree"></div>
+            <p className='para'>Already have an account? <span>Login here</span></p>
+            <input type="checkbox" />
+             <p>By continuing, i agree to the terms of use & privacy policy.</p>
+            {/* <div className="loginsignup-agree">
+              <input type="checkbox" />
+              <p>By continuing, i agree to the terms of use & privacy policy.</p>
+            </div> */}
+             </div>
+    </div>
+    
   )
 }
 
