c8ef501227426cd07f982530892767d73ef5a130
Akila Oshan Abenayaka <akilaabenayaka96@gmail.com>
2025-09-19 13:14:00 +0530

    decoration login page

--- frontend/src/Component/Pages/LoginSignup.jsx
+++ frontend/src/Component/Pages/LoginSignup.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import './Css/LoginSignup.css'
 
+
+
 function LoginSignup() {
   return (
-    <div className='loginsignup'>
+   <div className="loginsignup">
       <div className="loginsignup-container">
-            <h1>Sign Up</h1>
-            <input type="text" placeholder='Your Name' />
-            <input type="email" placeholder='Email Address' />
-            <input type="password" placeholder='Password' />
-     
-       <button>Continue</button> 
-       <div className="agree"></div>
-            <p className='para'>Already have an account? <span>Login here</span></p>
-            <input type="checkbox" />
-             <p>By continuing, i agree to the terms of use & privacy policy.</p>
-            {/* <div className="loginsignup-agree">
-              <input type="checkbox" />
-              <p>By continuing, i agree to the terms of use & privacy policy.</p>
-            </div> */}
-             </div>
+        <h1>Sign Up</h1>
+
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+
+        <button>Continue</button>
+
+        <p className="loginsignup-login">
+          Already have an account? <span>Login</span>
+        </p>
+
+        <div className="loginsignup-agree">
+          <input type="checkbox" id="agreeTerms" />
+          <p>
+            By continuing, I agree to the terms of use & privacy policy.
+          </p>
+        </div>
+      </div>
     </div>
     
   )
